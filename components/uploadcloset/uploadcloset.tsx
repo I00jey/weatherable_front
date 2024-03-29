dacaaddc5f5c5f6a2943105afb71d0fe42dd1d7d
wjdgns4070 <ljj888800@naver.com>
2024-03-29 17:53:22 +0900

    코디페이지 이상한거수정

--- components/uploadcloset/uploadcloset.tsx
+++ components/uploadcloset/uploadcloset.tsx
@@ -4,11 +4,9 @@ import styles from '../../styles/closet/closet.module.scss';
 import SelectBox from '../../components/closet/closet_main/selectBox';
 import SortBox from '../../components/closet/closet_main/sortBox';
 import ClothesInfoBox from '../../components/uploadcloset/uploadClosetInfoBox';
-
 import { useSelector } from 'react-redux';
-import { getUserClothes } from '../../service/closetApiService'; // 올바른 경로로 수정 필요
+import { getUserClothes } from '../../service/closetApiService';
 
-// ClothesInfoBoxComponent 컴포넌트의 속성 타입 정의
 interface ClosetPageProps {
   onImageSelect: (imageSrc: string, index: number) => void;
   clothes?: {
@@ -18,39 +16,54 @@ interface ClosetPageProps {
   };
 }
 
-// ClothesInfoBoxComponent 컴포넌트 정의
 const ClothesInfoBoxComponent: React.FC<ClosetPageProps> = ({
   onImageSelect,
 }) => {
-  // 사용자 옷 데이터와 정렬 상태를 관리하는 상태
   const [userClothesData, setUserClothesData] = useState<any[]>([]);
   const sortStatus = useSelector((state: any) => state.status.status);
+  const selectMajorData = useSelector((state: any) => state.search.selectMajor);
+  const selectMiddleData = useSelector(
+    (state: any) => state.search.selectMiddle
+  );
 
-  // 컴포넌트가 마운트될 때 사용자의 옷 데이터를 가져오는 효과
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await getUserClothes(); // 서버에서 사용자의 옷 데이터를 가져옵니다.
-        setUserClothesData(data); // 가져온 데이터로 상태를 업데이트합니다.
+        const data = await getUserClothes();
+        setUserClothesData(data);
       } catch (error) {
         console.error('사용자 옷 데이터를 불러오는 중 오류 발생:', error);
       }
     };
 
-    fetchData(); // 데이터를 가져오는 함수 호출
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    const filteredData = userClothesData.filter((item) => {
+      if (selectMajorData && selectMiddleData) {
+        return (
+          item.majorCategory === selectMajorData &&
+          item.middleCategory === selectMiddleData
+        );
+      } else if (selectMajorData && !selectMiddleData) {
+        return item.majorCategory === selectMajorData;
+      } else {
+        return true;
+      }
+    });
+
+    setUserClothesData(filteredData);
+  }, [selectMajorData, selectMiddleData, userClothesData]);
 
   return (
     <div className={styles.container}>
-      {/* 선택 상자 컴포넌트 렌더링 */}
       <div className={styles.selectBox}>
         <SelectBox />
       </div>
-      {/* 정렬 상자 컴포넌트 렌더링 */}
       <div className={styles.sortBox}>
         <SortBox />
       </div>
-      {/* 사용자의 옷 데이터를 표시하는 옷 정보 상자 컴포넌트 렌더링 */}
       <div
         className={
           sortStatus ? styles.mainInfoBoxDefault : styles.mainInfoBoxSmall
@@ -62,7 +75,7 @@ const ClothesInfoBoxComponent: React.FC<ClosetPageProps> = ({
             clothes={clothesItem}
             onImageSelect={(imageSrc: string, index: number) =>
               onImageSelect(imageSrc, index)
-            } // 이미지를 선택할 때 호출될 함수를 전달합니다.
+            }
           />
         ))}
       </div>
