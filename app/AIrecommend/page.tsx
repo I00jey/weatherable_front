0ec9716cb60819e4e9349ce646278cfe2e6b3b55
I00jey <cyj5056@gmail.com>
2025-08-05 22:11:40 +0900

    Fix: merged AIrecommend and airecommend folder conflict

--- app/AIrecommend/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const AiRecommendNoSSR = dynamic(() => import('../../components/AiRecommendComponent'), {
-  ssr: false,
-});
-
-export default AiRecommendNoSSR;
