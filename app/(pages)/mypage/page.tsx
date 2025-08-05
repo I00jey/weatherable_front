1a7e356ae57b1c2bf3fd235277f5f026256541a1
I00jey <cyj5056@gmail.com>
2025-08-05 21:38:16 +0900

    컴포넌트 경로 오류해결

--- app/(pages)/mypage/page.tsx
+++ app/(pages)/mypage/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-const MypageClient = dynamic(() => import('../../../components/mypage/mypageClient'), {
+const MypageClient = dynamic(() => import('../../../components/mypage/MypageClient'), {
   ssr: false
 });
 
--- components/MyPage/MypageClient.tsx
+++ components/MyPage/MypageClient.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import MypageHeader from './mypageHeader';
+import MypageHeader from './MypageHeader';
 import styles from '../../styles/mypage/mypage.module.scss';
 import PersonalInfo from './PersonalInfo';
 import Dimension from './Dimension';
